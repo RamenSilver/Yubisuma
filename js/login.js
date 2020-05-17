f28dab6bcf1aa29241f4b7fcd2a6330a212bf9ea
ai-sumikko <1222.ai.h@gmail.com>
2020-05-18 05:52:16 +0900

    Waiting画面の追加

--- js/login.js
+++ js/login.js
@@ -1,5 +1,17 @@
+$(document).on('click', '#icon',function(){
+  $("#modal-body").children().remove();
+  for (var i = 1; i < 65; i++) {
+    $('#modal-body').append('<img class="select-icon" src="./icon/icon'+i+'.png" alt="" data-dismiss="modal">');
+  }
+});
 
 $(document).on('click', '.select-icon',function(){
   let img = $(this).attr('src');
+  $('#icon').children('img').attr('src', img);
+});
+
+$(function(){
+  const randomIcon = Math.floor(Math.random() * Math.floor(64));
+  const img = "./icon/icon" + String(randomIcon+1) + ".png";
   $('#icon').children('img').attr('src', img);
 });
