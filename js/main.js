68836a5733fd107d5b2368a9572c311500a406a8
ai-sumikko <1222.ai.h@gmail.com>
2020-05-23 23:31:37 +0900

    main画面の枠追加

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function(){
   let count = countHands();
-  $('span').text(count);
+  $('.my-number1').find('span').text(count);
 
-//change
+  reset();
 
 });
 
@@ -34,10 +34,27 @@ function countUps() {
   });
   return count;
 }
+
+//指の総数
 function countHands() {
   let playersFingers = $(".fingers").find("img");
   let myFingers = $(".my-fingers").find("img");
   return playersFingers.length + myFingers.length;
+}
+
+//指の総数
+function countHandsUp() {
+  let count = 0;
+  let playersFingers = $(".fingers").find("img").attr("src").indexOf("Up");
+  let myFingers = $(".my-fingers").find("img").attr("src").indexOf("Up");
+  return playersFingers.length + myFingers.length;
+}
+
+function reset() {
+  $(".left").find("img").attr("src", "./img/leftDown.png");
+  $(".right").find("img").attr("src", "./img/rightDown.png");
+  $(".my-left").find("img").attr("src", "./img/myLeftDown.png");
+  $(".my-right").find("img").attr("src", "./img/myRightDown.png");
 }
 
 // モーダル 「いくつで勝負？」
@@ -53,6 +70,71 @@ $(document).on("click", "#select", function(){
 
 $(document).on("click", ".select", function(){
   let num = $(this).text();
-  $("#modal-body2 h2").text(num);
+
+  reset();
+  $(".text-one").text("");
+  $(".text-two").text("");
+
+  setTimeout(function(){
+    $(".text-one").text("いっせーのっせ！");
+  },1000);
+
+  setTimeout(function(){
+
+    userInf();
+    $('.fingersUpSum').find('span').text(countHandsUp());
+
+    $(".text-two").text(num);
+  },2500);
 
 });
+
+function userInf() {
+  // var ary = [
+  //   ["player1", "./img/leftUp.png", "./img/rightUp.png"],
+  //   ["player2", "./img/leftDown.png", "./img/rightDown.png"],
+  //   ["player3", "./img/leftUp.png", "./img/rightUp.png"],
+  //   ["player4", "./img/leftDown.png", "./img/rightDown.png"],
+  //   ["player5", "./img/leftUp.png", "./img/rightDown.png"],
+  //   ["player6", "./img/leftDown.png", "./img/rightUp.png"],
+  //   ["player7", "./img/leftUp.png", "./img/rightDown.png"],
+  //   ["player8", "./img/leftDown.png", "./img/rightDown.png"],
+  //   ["my", "./img/myLeftUp.png", "./img/myRightDown.png"],
+  // ];
+
+  var ary = [
+    ["player1", leftRdm(), rightRdm()],
+    ["player2", leftRdm(), rightRdm()],
+    ["player3", leftRdm(), rightRdm()],
+    ["player4", leftRdm(), rightRdm()],
+    ["player5", leftRdm(), rightRdm()],
+    ["player6", leftRdm(), rightRdm()],
+    ["player7", leftRdm(), rightRdm()],
+    ["player8", leftRdm(), rightRdm()],
+    ["my", "./img/myLeftUp.png", "./img/myRightDown.png"],
+  ];
+
+  for (var i = 0; i < ary.length; i++) {
+    $('.content').find('[name='+ary[i][0]+']').find(".left img,#left img").attr("src",ary[i][1]);
+    $('.content').find('[name='+ary[i][0]+']').find(".right img,#right img").attr("src",ary[i][2]);
+  }
+
+}
+
+function leftRdm() {
+  const randomIcon = Math.floor(Math.random() * Math.floor(2));
+  if (randomIcon==1) {
+    return "./img/leftUp.png"
+  } else {
+    return "./img/leftDown.png"
+  }
+}
+
+function rightRdm() {
+  const randomIcon = Math.floor(Math.random() * Math.floor(2));
+  if (randomIcon==1) {
+    return "./img/rightUp.png"
+  } else {
+    return "./img/rightDown.png"
+  }
+}
